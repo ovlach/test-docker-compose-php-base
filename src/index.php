a81f6cce9e077b81a824f8fb84cff438dbb0473d
Ondrej Vlach <ondrej@vlach.xyz>
2022-10-03 11:45:30 +0200

    MySQL usage + own extensions

--- src/index.php
+++ src/index.php
@@ -1,2 +1,4 @@
 <?php
+	$pdo = new PDO("mysql:host=db;dbname=mysql", "root", "mojeuzasneheslo");
 	echo "Juchů, ono to funguje na cca. 10 řádků :-D";
+	var_dump($pdo->query("SELECT * FROM `mysql`.`user`")->fetchall()); 
